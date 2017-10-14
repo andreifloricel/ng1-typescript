e7e7fd41691c45577c07933a8737d564bd56b89f
Saban Uenlue - netTrek <us@netTrek.de>
2017-10-14 09:41:19 +0200

    004_value_di

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,14 @@ import * as angular from 'angular';
 import { IModule } from 'angular';
 import { bindingModule } from './binding/binding.module';
 
-export const appModule: IModule = 
+export const appModule: IModule =
   angular.module('app', [ bindingModule.name ])
-      .run ( () => {
-          console.log ( 'run app' );
-      })
-      .config ( () => {
-          console.log ( 'config app' );
-      })
+
+         .run ( ( author: string ) => {
+             console.log ( 'run app', author );
+         })
+
+         .config ( () => {
+              console.log ( 'config app' );
+         })
 ;
--- src/app/binding/binding.module.ts
+++ src/app/binding/binding.module.ts
@@ -4,10 +4,15 @@ import { IModule } from 'angular';
 
 export const bindingModule: IModule = 
   angular.module('app.binding', [])
-         .run ( () => {
-             console.log ( 'run app.binding' );
+
+         .value( 'author', 'saban ünlü' )
+         .value( 'author', 'saban ünlü 2' )
+         .value( 'author', 'saban ünlü 3' )
+
+         .run ( ( author: string ) => {
+             console.log ( 'run app.binding', author );
          })
-         .config ( () => {
+         .config ( ( ) => {
              console.log ( 'config app.binding' );
          })
 ;
