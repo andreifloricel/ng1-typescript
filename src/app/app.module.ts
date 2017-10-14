8d5e18159ee78df0954c510e281a027d8f4918bf
Saban Uenlue - netTrek <us@netTrek.de>
2017-10-14 17:16:51 +0200

    015_root_parent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 
 import * as angular from 'angular';
-import { IModule } from 'angular';
+import { IModule, IRootScopeService } from 'angular';
 import { bindingModule } from './binding/binding.module';
 import "ngSanitize";
+
+
+interface IMyRoot extends IRootScopeService {
+    rootval: string;
+}
 
 export const appModule: IModule =
   angular.module('app', [ bindingModule.name, 'ngSanitize' ])
 
-         .run ( ( author: string ) => {
+         .run ( ( author: string, $rootScope: IMyRoot ) => {
              // console.log ( 'run app', author );
+             $rootScope.rootval = 'root';
          })
 
          .config ( () => {
