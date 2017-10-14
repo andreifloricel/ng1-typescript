db586edc6bc1d83e10ef596e15f8d35c48b7402f
Saban Uenlue - netTrek <us@netTrek.de>
2017-10-14 09:28:51 +0200

    003

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,9 @@
 //main entry point
+import * as angular from 'angular';
 import { appModule } from './app/app.module';
 
 console.log ( appModule.name );
+
+angular.element( document ).ready( ( ) => {
+   angular.bootstrap( document, [appModule.name] );
+});
