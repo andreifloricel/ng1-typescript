5431164dc6a9eca35641af5af68b872a6827e45c
Saban Uenlue - netTrek <us@netTrek.de>
2017-10-16 21:31:34 +0200

    appModule

---
+++ src/app/app.module.ts
@@ -0,0 +1,8 @@
+/**
+ * File created by suenlue on 16.10.17.
+ * Copyright (c) 2017 by netTrek GmbH & Co. KG
+ */
+import { IModule } from 'angular';
+import * as angular from 'angular';
+
+export const appModule: IModule   = angular.module( 'app', [] );
--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,4 @@
 //main entry point
 
-import { BMW, Car, sayHello } from './app/Car';
-import { NavButton } from './app/nav/NavButton';
-
-const car: Car = new Car ();
-const bmw: BMW = new BMW ();
-
-sayHello() ;
-
-const btn: HTMLButtonElement = <HTMLButtonElement>document.getElementById( 'btn');
-
-const navBtn: NavButton = new NavButton ( btn );
+import { appModule } from './app/app.module';
+console.log ( 'app module name', appModule.name );
