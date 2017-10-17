143890fb966ad5ceba66977f9c6854d6ac5f3204
Saban Uenlue - netTrek <us@netTrek.de>
2017-10-17 21:51:39 +0200

    051_component_ctrl

--- src/app/user/user-header/user-header.component.ts
+++ src/app/user/user-header/user-header.component.ts
@@ -2,4 +2,7 @@ import { IComponentOptions } from 'angular';
 
 export const UserHeaderComponet: IComponentOptions = {
     template: require ('./user-header.template.html!text'),
+    controller: class UserHeaderController {
+        title = 'User Header Works with binding'
+    }
 };
