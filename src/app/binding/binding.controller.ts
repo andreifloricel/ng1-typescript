526092058e8e83623e4643e37806a1d4d302f2a7
Saban Uenlue - netTrek <us@netTrek.de>
2017-10-17 19:11:08 +0200

    style_and_so_on

--- src/app/binding/binding.controller.ts
+++ src/app/binding/binding.controller.ts
@@ -7,7 +7,13 @@ import { IOnInit } from 'angular';
 console.log ( 'BindingCtr loaded' );
 export default class BindingController implements IOnInit {
 
+    imgFolder: string = 'images';
+    imgFile: string = 'image-881881-galleryV9-htln-881881.jpg';
+    link: string = 'http://netTrek.de';
+
+    progress: number = 22;
     name: string;
+    checked: boolean = false;
     modelOpt: any = {
         updateOn: 'default blur',
         debounce: {
@@ -16,11 +22,28 @@ export default class BindingController implements IOnInit {
         }
     };
 
+    className: string = 'red';
+
     constructor ( private author: string ) {
 
         console.log ( 'BindingCtr build', author );
     }
-    
+
+    checkYellow (): Boolean {
+        return this.name === '';
+    }
+
+    cbChg ( checked: boolean) {
+        console.log ( 'checked', checked );
+        if ( checked === true ) {
+            this.className = 'yellow';
+            this.progress = 70;
+        } else {
+            this.className = 'red';
+            this.progress = 50;
+        }
+    }
+
     $onInit (): void {
         console.log ( 'ctrl is init' );
         this.name = `saban ${this.author}`
@@ -30,9 +53,22 @@ export default class BindingController implements IOnInit {
         return `<strong>${prefix}</strong><script>alert("hh")</script> ${this.name}`;
     }
 
+    onPast ( evt: Event ) {
+        console.log ( evt, this.name );
+    }
+    
     onClick ( msg:  string, event: Event ) {
         console.log ( 'click', event );
         this.name = this.name.toUpperCase();
     }
+
+    keyUp ( keyEvent: Event ) {
+        console.log ( 'keyEvent', keyEvent);
+    }
+
+    txtChg ( msg: string ) {
+        console.log ( 'msg', msg );
+    }
+
 
 }
